645e3aa5aa0e9b8d7443ab9046b571c798357261
SZkristof <szabokristof0@gmail.com>
2017-03-23 23:58:24 +0100

    IDLE working, VS code not

--- gameInventory.py
+++ gameInventory.py
@@ -1,4 +1,4 @@
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12, 'kilmandzsaro': 25}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def display_inventory(inventory):
@@ -14,63 +14,49 @@ def add_to_inventory(inventory, added_items):
             inventory.update({i:1})
 
 def print_table(inv, order=None):
-    print("Inventory:")
-    count = "count"
-    print(f'{count:>8}')
     
-    b = max(len(x) for x in inv) + 5
-    asdf = []
-    for i in inv:
-        asdf.append(inv[i])
-    c = len(str(max(asdf)))
-    print('-'*(b+c+1))
-
+    n = max(len(x) for x in inventory)
+    print ("Inventory:")
+    print ("count" + " "*n + "Items' name" + "\n" + "-"*n*3)
+    b = max(len(x) for x in inventory)*2
     if order == "count,desc":
-        a = sorted(inv, key=inv.get, reverse=True)
+        a = sorted(inventory,key=inventory.get, reverse=True)
         for i in a:
-            placeholder = '{count:>{0}} {item:>' + str(b) + '}'
-            print(placeholder.format(str(c),count=str(inv[i]), item=i))
+            placeholder = ' {count:>5} {item:>' + str(b) + '}'
+            print (placeholder.format(count=str(inventory[i]), item=i))
     elif order == "count,asc":
-        a = sorted(inv, key=inv.get)
+        a = sorted(inventory, key=inventory.get)
         for i in a:
-            print(str(inv[i]) + " " + i)
+            print(str(inventory[i]) + " " + i)
     elif None:
         pass
     else:
         pass
-
+    print("-"*n*3)
 '''
-def print_table(inventory, order=None):
-    n = max(len(x) for x in inventory)
-    m = 30-n
-    print ("Inventory:" + "\ncount item_name" + "\n" + "-"*m)
-    if order == "count,asc":
-        a = sorted(inventory, key=inventory.get)
-        print (str(inventory[i]) + " "*n + i)
-    elif order == "count,desc":
-        a = sorted(inventory, key=inventory.get, reverse=True)
-        for i in a:
-            print(str(inventory[i]) + " "*n + i)
-    elif order == None:
-        return display_inventory(inventory)
-    else:
-        print ("You are not worthy to peek into this mighty inventory!")
-        pass
-'''    
+def import_inventory(inventory, filename="import_inventory.csv"):
+    import csv
+    with open(filename) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            import_inventory = (row)
+            for i in import_inventory:
+                if i in inventory:
+                    inventory[i] += 1
+                else:
+                    inventory.update({i:1})
+    pass
 
+
+def export_inventory(inventory, filename="export_inventory.csv"):
+    import csv
+
+    with open('export_inventory.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(inventory)
+    pass
+'''
+#import_inventory(inventory, filename="test_inventory.csv")
 add_to_inventory(inventory, dragon_loot)
 print_table(inventory, "count,desc")
-# Imports new inventory items from a file
-# The filename comes as an argument, but by default it's 
-# "import_inventory.csv". The import automatically merges items by name.
-# The file format is plain text with comma separated values (CSV).
-def import_inventory(inventory, filename="import_inventory.csv"):
-    pass
-
-
-# Exports the inventory into a .csv file.
-# if the filename argument is None it creates and overwrites a file
-# called "export_inventory.csv". The file format is the same plain text 
-# with comma separated values (CSV).
-def export_inventory(inventory, filename="export_inventory.csv"):
-    pass
+#export_inventory(inventory, filename="test_inventory_export.csv")
