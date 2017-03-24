df30a7f774428b5fe7c0ca3a3e9f5d3aff8079b2
SZkristof <szabokristof0@gmail.com>
2017-03-24 18:16:38 +0100

    Minor bugs and features to add. Ready to submit

--- gameInventory.py
+++ gameInventory.py
@@ -1,74 +1,73 @@
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} 
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def display_inventory(inventory):
-    print("Inventory:")
-    item_total=0
-    for k, v in inventory.items():
-        print(v, k)
-    print ("Total number of items:  " + str(sum(inventory.values())))
-
+    for first_item, second_item in inventory.items():
+        print (second_item, first_item)
 
 
 def add_to_inventory(inventory, added_items):
     for i in added_items:
-        if i in inventory:
+        if i in inventory: #megnézi, 'i' benne van e az inventoryban és ha igen, felveszi az értékét
             inventory[i] += 1
         else:
             inventory.update({i:1})
-
-
 def print_table(inv, order=None):
-    b = max(len(x) for x in inv) + 5
-    print("Inventory:")
-    count = "count".ljust(10," ")
-    print(f'{count:>8}'+'Item name'.rjust(b-7, " "))
-    asdf = []
-    for i in inventory:
-        asdf.append(inventory[i])
-    c = len(str(max(asdf)))
-    print('-'*(b+c+1))
+    
+    n = max(len(x) for x in inventory)
+    print ("Inventory:")
+    print ("count" + " "*n + "Items' name" + "\n" + "-"*n*3)
+    b = max(len(x) for x in inventory)*2
     if order == "count,desc":
-        a = sorted(inventory, key=inventory.get, reverse=True)
+        a = sorted(inventory,key=inventory.get, reverse=True)
         for i in a:
-            placeholder = '{count:>{0}} {item:>' + str(b) + '}'
-            print(placeholder.format(str(c),count=str(inventory[i]), item=i))
-        print('-'*(b+c+1))
-        print ("Total number of items:  " + str(sum(inventory.values())))
+            placeholder = ' {count:>5} {item:>' + str(b) + '}'
+            print (placeholder.format(count=str(inventory[i]), item=i))
     elif order == "count,asc":
         a = sorted(inventory, key=inventory.get)
         for i in a:
             print(str(inventory[i]) + " " + i)
-        print('-'*(b+c+1))
-        print ("Total number of items:  " + str(sum(inventory.values())))
     elif None:
         pass
     else:
         pass
+    print("-"*n*3)
 
-def import_inventory(inventory, filename="test_inventory.csv"):
+
+
+# Imports new inventory items from a file
+# The filename comes as an argument, but by default it's 
+# "import_inventory.csv". The import automatically merges items by name.
+# The file format is plain text with comma separated values (CSV).
+def import_inventory(inventory, filename):
     import csv
     with open(filename) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
             import_inventory = (row)
             for i in import_inventory:
-                if i in inventory:
+                if i in inventory: #megnézi, 'i' benne van e az inventoryban és ha igen, felveszi az értékét
                     inventory[i] += 1
                 else:
                     inventory.update({i:1})
     pass
 
-def export_inventory(inventory, filename="export_inventory.csv"):
+
+
+# Exports the inventory into a .csv file.
+# if the filename argument is None it creates and overwrites a file
+# called "export_inventory.csv". The file format is the same plain text 
+# with comma separated values (CSV).
+def export_inventory(inventory, filename):
     import csv
 
-    with open('export_inventory.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(inventory)
     pass
 
 
+import_inventory(inventory, filename="test_inventory.csv")
 add_to_inventory(inventory, dragon_loot)
 print_table(inventory, "count,desc")
-import_inventory(inventory)
-export_inventory(inventory)
+export_inventory(inventory,filename="test_inventory_export.csv")
