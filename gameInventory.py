9c0ede68a4311c1ec575275c262256969c5cbd12
SZkristof <szabokristof0@gmail.com>
2017-03-24 11:21:44 +0100

    Working, need some fix(1 failure, 1 error)

--- gameInventory.py
+++ gameInventory.py
@@ -1,40 +1,52 @@
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12, 'kilmandzsaro': 25}
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} 
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def display_inventory(inventory):
-    for first_item, second_item in inventory.items():
-        print (second_item, first_item)
+    print("Inventory:")
+    item_total=0
+    for k, v in inventory.items():
+        print(v, k)
+    print ("Total number of items:  " + str(sum(inventory.values())))
+
 
 
 def add_to_inventory(inventory, added_items):
     for i in added_items:
-        if i in inventory: #megnézi, 'i' benne van e az inventoryban és ha igen, felveszi az értékét
+        if i in inventory:
             inventory[i] += 1
         else:
             inventory.update({i:1})
 
+
 def print_table(inv, order=None):
-    
-    n = max(len(x) for x in inventory)
-    print ("Inventory:")
-    print ("count" + " "*n + "Items' name" + "\n" + "-"*n*3)
-    b = max(len(x) for x in inventory)*2
+    b = max(len(x) for x in inv) + 5
+    print("Inventory:")
+    count = "count".ljust(10," ")
+    print(f'{count:>8}'+'Item name'.rjust(b-7, " "))
+    asdf = []
+    for i in inventory:
+        asdf.append(inventory[i])
+    c = len(str(max(asdf)))
+    print('-'*(b+c+1))
     if order == "count,desc":
-        a = sorted(inventory,key=inventory.get, reverse=True)
+        a = sorted(inventory, key=inventory.get, reverse=True)
         for i in a:
-            placeholder = ' {count:>5} {item:>' + str(b) + '}'
-            print (placeholder.format(count=str(inventory[i]), item=i))
+            placeholder = '{count:>{0}} {item:>' + str(b) + '}'
+            print(placeholder.format(str(c),count=str(inventory[i]), item=i))
+        print('-'*(b+c+1))
+        print ("Total number of items:  " + str(sum(inventory.values())))
     elif order == "count,asc":
         a = sorted(inventory, key=inventory.get)
         for i in a:
             print(str(inventory[i]) + " " + i)
+        print('-'*(b+c+1))
+        print ("Total number of items:  " + str(sum(inventory.values())))
     elif None:
         pass
     else:
         pass
-    print("-"*n*3)
-'''
-def import_inventory(inventory, filename="import_inventory.csv"):
+
+def import_inventory(inventory, filename="test_inventory.csv"):
     import csv
     with open(filename) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
@@ -47,7 +59,6 @@ def import_inventory(inventory, filename="import_inventory.csv"):
                     inventory.update({i:1})
     pass
 
-
 def export_inventory(inventory, filename="export_inventory.csv"):
     import csv
 
@@ -55,8 +66,9 @@ def export_inventory(inventory, filename="export_inventory.csv"):
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(inventory)
     pass
-'''
-#import_inventory(inventory, filename="test_inventory.csv")
+
+
 add_to_inventory(inventory, dragon_loot)
 print_table(inventory, "count,desc")
-#export_inventory(inventory, filename="test_inventory_export.csv")
+import_inventory(inventory)
+export_inventory(inventory)
