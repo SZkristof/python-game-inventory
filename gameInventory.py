44073a4eb169972b9a9120db3277e3f64f2c0417
SZkristof <szabokristof0@gmail.com>
2017-03-26 16:34:38 +0200

    Ready

--- gameInventory.py
+++ gameInventory.py
@@ -1,19 +1,28 @@
+import csv
+
+
+
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
+
+
 def display_inventory(inventory):
-    for first_item, second_item in inventory.items():
-        print (second_item, first_item)
+    for k, v in inventory.items():
+        print (v, k)
+
 
 
 def add_to_inventory(inventory, added_items):
     for i in added_items:
-        if i in inventory: #megnézi, 'i' benne van e az inventoryban és ha igen, felveszi az értékét
+        if i in inventory:
             inventory[i] += 1
         else:
             inventory.update({i:1})
-def print_table(inv, order=None):
-    
+
+
+
+def print_table(inventory, order=None):   
     n = max(len(x) for x in inventory)
     print ("Inventory:")
     print ("count" + " "*n + "Items' name" + "\n" + "-"*n*3)
@@ -22,49 +31,38 @@ def print_table(inv, order=None):
         a = sorted(inventory,key=inventory.get, reverse=True)
         for i in a:
             placeholder = ' {count:>5} {item:>' + str(b) + '}'
-            print (placeholder.format(count=str(inventory[i]), item=i))
+            print (placeholder.format(count=str(inventory[i]), item=i))       
     elif order == "count,asc":
         a = sorted(inventory, key=inventory.get)
         for i in a:
-            print(str(inventory[i]) + " " + i)
+            print(str(inventory[i]) + " " + i)    
     elif None:
         pass
     else:
         pass
     print("-"*n*3)
+    print("Total number of items: " + str(sum(inventory.values())) + "\n") 
 
 
 
-# Imports new inventory items from a file
-# The filename comes as an argument, but by default it's 
-# "import_inventory.csv". The import automatically merges items by name.
-# The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename):
-    import csv
     with open(filename) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
             import_inventory = (row)
             for i in import_inventory:
-                if i in inventory: #megnézi, 'i' benne van e az inventoryban és ha igen, felveszi az értékét
+                if i in inventory:
                     inventory[i] += 1
                 else:
                     inventory.update({i:1})
-    pass
 
 
 
-# Exports the inventory into a .csv file.
-# if the filename argument is None it creates and overwrites a file
-# called "export_inventory.csv". The file format is the same plain text 
-# with comma separated values (CSV).
 def export_inventory(inventory, filename):
-    import csv
-
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(inventory)
-    pass
+
 
 
 import_inventory(inventory, filename="test_inventory.csv")
